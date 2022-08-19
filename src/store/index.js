4da482f30a0cfad2c99b24d4fe41661e673bf563
LiuXiaoying <2214872361@qq.com>
2022-08-19 15:45:47 +0800

    修改了router和store下单index.js文件，添加了两个配置文件jsconfig.json和eslintignore

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,18 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // 数据
   state: {
   },
+  // 改数据函数
   mutations: {
   },
+  // 请求数据函数
   actions: {
   },
+  // 分模块
   modules: {
-  }
+  },
+  // vuex的计算属性
+  getters: {}
 })
