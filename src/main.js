52062aee104faaad7ed796db7215b16b8816b261
LiuXiaoying <2214872361@qq.com>
2022-08-20 14:25:14 +0800

    less自动导入

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 安装重置样式的包
+import 'normalize.css'
+// 项目公共样式
+import '@/assets/styles/common.less'
+
 const app = createApp(App)
 app.use(store)
 app.use(router)
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// vue add style-resources-loader 通过vue-resources-loader完成代码注入再每个less文件和vue组件中。
+const path = require('path')
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.join(__dirname, './src/assets/styles/variables.less'),
+        path.join(__dirname, './src/assets/styles/mixins.less')
+      ]
+    }
+  }
+}
