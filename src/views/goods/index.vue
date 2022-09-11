183bffca7e840b2a6e975a9292b20d5617c172c0
LiuXiaoying <2214872361@qq.com>
2022-09-11 16:55:48 +0800

    规格组件的禁用效果

--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -16,6 +16,7 @@
         </div>
         <div class="spec">
           <GoodsName :goods="goods" />
+          <GoodsSku :goods="goods" />
         </div>
       </div>
       <!-- 商品推荐 -->
@@ -36,6 +37,7 @@
 </template>
 
 <script>
+import GoodsSku from './components/goods-sku.vue'
 import GoodsSales from './components/goods-sales'
 import GoodsName from './components/goods-name'
 import GoodsImage from './components/goods-image.vue'
@@ -45,7 +47,7 @@ import { findGoods } from '@/api/product'
 import { useRoute } from 'vue-router'
 export default {
   name: 'XtxGoodsPage',
-  components: { GoodsRelevant, GoodsImage, GoodsSales, GoodsName },
+  components: { GoodsRelevant, GoodsImage, GoodsSales, GoodsName, GoodsSku },
   setup() {
     const goods = useGoods()
     // provide('goods', goods)
